c45352c3a7cc9f6195d08d02396fd7af203b4cfa
Matin Bayram <imatinbayram@gmail.com>
2026-04-13 15:12:15 +0400

    Update Client.py

--- Client.py
+++ Client.py
@@ -12,12 +12,12 @@ warnings.simplefilter("ignore")
 # -----------------------------
 def borc():
     today = date.today()
-    #tarix_1 = today.replace(day=1).isoformat()
+    tarix_1 = today.replace(day=1).isoformat()
     tarix_2 = today.isoformat()
     with open("Borc.sql", encoding="utf-8") as f:
         query_text = f.read().lstrip('\ufeff')
     query = f"""
-        DECLARE @tarix1 DATE = '{tarix_2}';
+        DECLARE @tarix1 DATE = '{tarix_1}';
         DECLARE @tarix2 DATE = '{tarix_2}';
         {query_text}
     """
